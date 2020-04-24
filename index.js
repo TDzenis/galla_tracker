5ae0cd30cf8b65077a5df604c3cca3f009ba03f2
TDzenis <Tomass.Dzenis@outlook.com>
2020-04-24 12:16:11 +0100

    Login/register pages basic func done

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const port = 3000
+
 const fs = require('fs')
 const mysql = require('mysql');
+
 const bcrypt = require('bcrypt')
-const dbSettings = require('./config.json')
 const saltRounds = 10
+
+const dbSettings = require('./config.json')
 
 const con = mysql.createConnection(dbSettings);
 
@@ -22,6 +26,35 @@ app.get('/login', (req, res) => {
     res.write(data);
     return res.end();
   });
+})
+  
+app.post('/logInUser', (req, res) => {
+  let con = mysql.createConnection(dbSettings);
+  let password = req.body.user.password;
+  let email = req.body.user.email;
+  let checkUserSql = "SELECT * FROM user WHERE email = ?;";
+  let getPasswordSql = "SELECT password FROM user WHERE email = ?;"
+
+  con.connect((err) => {
+    if (err) throw err;
+    con.query(checkUserSql, email, (err, result) => { 
+      if (err) throw err;
+      if (result.length > 0) { 
+        con.query(getPasswordSql, email, (err, result) => { 
+          if (err) throw err;
+          bcrypt.compare(password, result[0].password).then((result) => {
+            if (result) {
+              returnJson(res, "ok", "");
+            } else {
+              returnJson(res, "fail", "Password is not correct");
+            }
+          })
+        })
+      } else {
+        returnJson(res, "fail", "User does not exist, please register.");
+      }
+    })
+  })
 })
 
 app.get('/register', (req, res) => {
@@ -43,7 +76,7 @@ app.post('/registerUser', (req, res) => {
     if (err) throw err;
     
     bcrypt.hash(password, saltRounds, (err, hash) => {
-      const checkUserSql = "SELECT * FROM user WHERE email = ?;";
+      let checkUserSql = "SELECT * FROM user WHERE email = ?;";
       const registerUserSql = "INSERT INTO user(email, password) VALUES (?);";
       const hashedPassword = "";
       const newUser = [email, hash];
