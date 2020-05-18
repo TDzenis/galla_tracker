c8028b0ba04a43b1ca18a035e911c9a417e3860e
TDzenis <Tomass.Dzenis@outlook.com>
2020-05-18 10:01:02 +0100

    small edit

--- js/main.js
+++ js/main.js
@@ -10,12 +10,11 @@ var ticketApp = new Vue({
           return response.json()
         })
         .then(data => {
-          //assigns JSON to variable "tickets"
-          //console.log(data)
+          //assigns data from JSON to variable "tickets"
           ticketApp.tickets = data;
         })
         .catch(err => {
-          console.log(err);
+          console.error(err);
         })
     },
     saveChanges: (id) => {
@@ -23,21 +22,11 @@ var ticketApp = new Vue({
       let form = document.getElementById(formId);
       const ticketName = form.getElementsByClassName("ticket_name");
       const ticketDescription = form.getElementsByClassName("ticket_description");
-      //const ticketDeadline = form.getElementsByClassName("ticket_deadline");
-      //const ticketAssignedTo = document.getElementsByClassName("ticket_assigned_to");
-      //const ticketStatus = document.getElementsByClassName("ticket_status");
-      //const ticketImportance = document.getElementsByClassName("ticket_importance");
-      //const ticketEstimatedTimeNeeded = document.getElementsByClassName("ticket_estimated_time_needed");
       
       const ticket = {
         ticketId: id,
         ticketName: ticketName[0].value,
-        ticketDescription: ticketDescription[0].value,
-        //ticketDeadline : ticketDeadline[0].value,
-        //ticketAssignedTo: ticketAssignedTo[0].value,
-        //ticketStatus: ticketStatus[0].value,
-        //ticketEstimatedTimeNeeded: ticketEstimatedTimeNeeded[0].value,
-        //ticketImportance: ticketImportance[0].value
+        ticketDescription: ticketDescription[0].value
       }
 
       console.log(ticket);
@@ -56,7 +45,6 @@ var ticketApp = new Vue({
     }
   },
   mounted() {
-    console.log('Component has been mounted!');
     this.getTickets();
   }
 })
